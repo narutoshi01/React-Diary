2d12fef207c0f4651d7ed1e7e664c589b0ac422b
sakamichi-jewel <taigayyy@gmail.com>
2019-12-30 17:57:40 +0900

    add Toolbar

---
+++ src/components/Toolbar/Toolbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Toolbar = props => {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>Diary</li>
+          <li>Show All Dairy</li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Toolbar;
